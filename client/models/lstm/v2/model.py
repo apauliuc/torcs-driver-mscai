27aba3c8db6a61f94665d23a430b148b254a2bcb
Andrei Sili <andreisili@BestVM.zjolrppqgsqetejrpxhrx2zd0h.ax.internal.cloudapp.net>
2017-12-09 03:15:55 +0000

    v2.1

--- client/models/lstm/v2/model.py
+++ client/models/lstm/v2/model.py
@@ -20,37 +20,56 @@ else:
 
 
 class LSTMDriver(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, output_size, batch_size):
+    def __init__(self, input_size, lstm_hidden_size, hidden_layer_size, dropout_prob, num_layers, output_size, batch_size):
         super(LSTMDriver, self).__init__()
         self.input_size = input_size
-        self.hidden_size = hidden_size
+        self.lstm_hidden_size = lstm_hidden_size
+        self.hidden_layer_size = hidden_layer_size
+        self.dropout_prob = dropout_prob
+        
         self.num_layers = num_layers
         self.output_size = output_size
         self.batch_size = batch_size
         
         super(LSTMDriver, self).__init__()
+        
+        self.hidden_state = self.init_hidden()
 
-        self.lstm = nn.LSTM(input_size=self.input_size, hidden_size=self.hidden_size, num_layers=self.num_layers)
+        self.lstm = nn.LSTM(input_size=self.input_size, hidden_size=self.lstm_hidden_size, num_layers=self.num_layers)
         
-        self.hidden = self.init_hidden()
+        self.hidden = nn.Linear(self.lstm_hidden_size, self.hidden_layer_size)
         
-        self.linear = nn.Linear(self.hidden_size, self.output_size)
+        self.sigmoid = nn.Sigmoid()
+        
+        self.dropout = nn.Dropout(p=dropout_prob)
+        
+        self.out = nn.Linear(self.hidden_layer_size, self.output_size)
 
     def init_hidden(self):
-        return (autograd.Variable(torch.zeros(self.num_layers, self.batch_size, self.hidden_size)).type(DTYPE),
-                    autograd.Variable(torch.zeros(self.num_layers, self.batch_size, self.hidden_size)).type(DTYPE))
+        return (autograd.Variable(torch.zeros(self.num_layers, self.batch_size, self.lstm_hidden_size)).type(DTYPE),
+                    autograd.Variable(torch.zeros(self.num_layers, self.batch_size, self.lstm_hidden_size)).type(DTYPE))
     
     def forward(self, x):
-        lstm_out, self.hidden = self.lstm(x.view(len(x), 1, -1), self.hidden)
-        linear_out = self.linear(lstm_out)
+        lstm_out, self.hidden_state = self.lstm(x.view(len(x), 1, -1), self.hidden_state)
         
-        return linear_out
+        hidden_out = self.sigmoid(self.hidden(lstm_out))
+
+        drop_out = self.dropout(hidden_out)
+        
+        out = self.out(drop_out)
+        
+        return out
     
 class HYPERPARAMS(object):
     TARGET_SIZE = 3
     INPUT_SIZE = 22
-    HIDDEN_SIZE = 75
+    LSTM_HIDDEN_SIZE = 200
+    HIDDEN_LAYER_SIZE = 100
+    DROPOUT_PROB = 0.2
     NUM_LAYERS = 2
+    
+    
     BATCH_SIZE = 1
     NUM_EPOCHS = 150
     LEARNING_RATE = 1e-3
+    
